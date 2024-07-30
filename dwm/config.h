0345fa454ca1ee46c8b290c5bc0779a2a497a2d6
Aditya Gautam <adityascottish27@gmail.com>
2024-07-30 16:43:06 +0530

    Updated

--- dwm/config.h
+++ dwm/config.h
@@ -27,8 +27,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-// static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "dev", "www", "sys", "doc", "pdf", "mus", "key", "mcu", "vid" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+// static const char *tags[] = { "dev", "www", "sys", "doc", "pdf", "mus", "key", "mcu", "vid" };
 
 static const Rule rules[] = {
 	/* xprop(1):
